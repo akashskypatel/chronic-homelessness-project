0ee2885fd89c176dbedcb378c61fad79abf04c18
Josh <98499717+KohetsuCoding@users.noreply.github.com>
2022-11-23 10:39:44 -0500

    Added authorization with Auth0
    Preliminary commit with changes to Rest API security

---
+++ rest-api/src/main/java/com/cityoforlando/restapi/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cityoforlando.restapi.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/public").permitAll()
+                .mvcMatchers("/private").authenticated()
+                .mvcMatchers("/private-scoped").hasAuthority("SCOPE_read:messages")
+                .and().cors()
+                .and().oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
