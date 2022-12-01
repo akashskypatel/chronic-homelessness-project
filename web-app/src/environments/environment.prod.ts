5a7827616b720cd437dbe7a60df6ce7e39a8b77c
akashskypatel <akashskypatel@gmail.com>
2022-12-01 18:41:05 -0500

    finalize

--- web-app/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  google_maps_api_key: '' //enter your Google Maps API key here
-};
--- web-app/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  google_maps_api_key: '' //enter your Google Maps API key here
-};
